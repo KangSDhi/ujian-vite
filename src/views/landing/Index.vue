2864d34aac9614aa3feceafcb8805c7481bd844b
KangSDhi <kangteknisi@gmail.com>
2023-05-21 08:42:54 +0700

    edit

--- src/views/landing/Index.vue
+++ src/views/landing/Index.vue
@@ -5,25 +5,14 @@
                 <img
                 alt="nature"
                 class="h-[32rem] w-full object-cover object-center"
-                src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2717&amp;q=80"
+                src="../../../public/img/assets-cbt.jpg"
                 />
             </div>
             <h2 class="mb-2 block font-sans text-4xl font-semibold leading-[1.3] tracking-normal text-blue-gray-900 antialiased">
-                What is Material Tailwind
+                Selamat Datang Di Aplikasi Ujian
             </h2>
             <p class="block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
-                Can you help me out? you will get a lot of free exposure doing this can my
-                website be in english?. There is too much white space do less with more,
-                so that will be a conversation piece can you rework to make the pizza look
-                more delicious other agencies charge much lesser can you make the blue
-                bluer?. I think we need to start from scratch can my website be in
-                english?, yet make it sexy i'll pay you in a week we don't need to pay
-                upfront i hope you understand can you make it stand out more?. Make the
-                font bigger can you help me out? you will get a lot of free exposure doing
-                this that's going to be a chunk of change other agencies charge much
-                lesser. Are you busy this weekend? I have a new project with a tight
-                deadline that's going to be a chunk of change. There are more projects
-                lined up charge extra the next time.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam amet debitis explicabo consequatur molestias autem esse porro ad perspiciatis sequi vel distinctio, enim saepe sunt ducimus! Provident enim dignissimos hic.
             </p>
         </template>
     </BaseLayout>
