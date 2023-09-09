4747dcbf1cb839008f81372750b79614ed77f024
KangSDhi <kangteknisi@gmail.com>
2023-09-09 08:31:41 +0700

    feat(web): dashboard admin on progress

--- src/views/admin/dashboard/layouts/BaseLayout.vue
+++ src/views/admin/dashboard/layouts/BaseLayout.vue
@@ -1,48 +1,115 @@
 <template>
-    <div>
-        <section>
-            <main>
-                <slot name="content"></slot>
-            </main>
-        </section>
-    </div>
+  <div>
+    <section>
+      <button @click="toggleSidebar" type="button"
+              class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        <span class="sr-only">Open sidebar</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+             xmlns="http://www.w3.org/2000/svg">
+          <path clip-rule="evenodd" fill-rule="evenodd"
+                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+        </svg>
+      </button>
+      <aside class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform"
+             :class="sidebarComputed">
+        <div class="h-full px-3 py-4 bg-gray-50 dark:bg-gray-800">
+          <a href="#" class="flex items-center pl-2.5 mb-5">
+            <img src="../../../../../public/img/logo.webp" class="h-5 mr-3 sm:h-7" alt="Ujian Logo"/>
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Sistem Ujian</span>
+          </a>
+          <button @click="toggleSidebar" type="button"
+                  class="sm:invisible text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                 xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"></path>
+            </svg>
+            <span class="sr-only">Close menu</span>
+          </button>
+          <ul>
+            <li>
+              <a href="#"
+                 class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <svg
+                    class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z"/>
+                </svg>
+                <span class="ml-3">Dashboard</span>
+              </a>
+            </li>
+            <li>
+              <a href="#"
+                 class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <svg
+                    class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                  <path
+                      d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
+                </svg>
+                <span class="flex-1 ml-3 whitespace-nowrap">Users</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+      <main class="p-4 sm:ml-64">
+        <slot name="content"></slot>
+      </main>
+    </section>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
 
 export default {
-    data(){
-        return {
-            URL: import.meta.env.VITE_API_URL
-        }
-    },
-    mounted(){
-        this.initAuth();
-    },
-    methods: {
-        initAuth(){
-            const token = localStorage.getItem('token_auth');
-            if (token == null) {
-                this.$router.push({name: 'Landing'});
-            }else{
-                axios.get(this.URL + 'check_auth', {
-                    headers: {
-                        'Authorization': 'Bearer ' + token
-                    }
-                })
-                .then(({ data }) => {
-                    if (data.success == true) {
-                        console.log({ status: 'Auth Valid' });   
-                    }
-                })
-                .catch(({ response }) => {
-                    if (response.status == 401) {
-                        this.$router.push({name: 'Landing'});
-                    }
-                });
-            }
-        }
+  data() {
+    return {
+      URL: import.meta.env.VITE_API_URL,
+      isSideBarOpen: false,
     }
+  },
+  mounted() {
+    this.initAuth();
+  },
+  computed: {
+    sidebarComputed(){
+      return this.isSideBarOpen ? '' : '-translate-x-full sm:translate-x-0';
+    },
+    activeNavigationComputed(){
+
+    }
+  },
+  methods: {
+    initAuth() {
+      const token = localStorage.getItem('token_auth');
+      if (token == null) {
+        this.$router.push({name: 'Landing'});
+      } else {
+        axios.get(this.URL + 'check_auth', {
+          headers: {
+            'Authorization': 'Bearer ' + token
+          }
+        })
+            .then(({data}) => {
+              if (data.success === true) {
+                console.log({status: 'Auth Valid'});
+              }
+            })
+            .catch(({response}) => {
+              if (response.status === 401) {
+                this.$router.push({name: 'Landing'});
+              }
+            });
+      }
+    },
+    toggleSidebar() {
+      this.isSideBarOpen = !this.isSideBarOpen
+    }
+  }
 }
 </script>
